bd75e15e249f79d4a8ad840c5001dfd7bba14852
Elvin Husubeyli <husubayli@gmail.com>
2021-02-15 01:57:54 +0400

    receive response data

--- index/static/js/product-filter.js
+++ index/static/js/product-filter.js
@@ -1,69 +1,76 @@
-// console.log('salam');
-// $(document).ready(function(){
-// 	$(".ajaxLoader").hide();
-// 	$(".filter-item-checkbox").on('click',function(){
-//         console.log($(this));
-// 		var _filterObj={};
-// 		$(".filter-item-checkbox").each(function(index,ele){
-// 			var _filterVal=$(this).val();
-//             // console.log(_filterVal);
-// 			var _filterKey=$(this).data('filter');
-//             console.log();
+console.log('salam');
+$(document).ready(function(){
+	$(".ajaxLoader").hide();
+	$(".filter-item-checkbox").on('click',function(){
+        console.log($(this));
+		var _filterObj={};
+		$(".filter-item-checkbox").each(function(index,ele){
+			var _filterVal=$(this).val();
+            // console.log(_filterVal);
+			var _filterKey=$(this).data('filter');
+            console.log();
 
-// 			_filterObj[_filterKey]=Array.from(document.querySelectorAll('input[data-filter='+_filterKey+']:checked')).map(function(el){
-// 			    return el.value;
-// 			});
-// 		});
-//         console.log(_filterObj, 'obyekt');
-// 		// Run Ajax
-// 		$.ajax({
-// 			url: "http://localhost:8000/az/product/ajax/filter-data/",
-// 			data: _filterObj,
-// 			dataType:'json',
-// 			beforeSend:function(){
-//                 console.log('necesen');
-// 				$(".ajaxLoader").show();
-// 			},
-// 			success:function(res){
-// 				console.log(res.data);
-// 				$("#filteredProducts").html(res.data);
-// 				$(".ajaxLoader").hide();
-// 			},
-//             error: function(res){
-//                 console.log(res, 'error');
-//             }
-// 		});
-// 	});
-// });
+			_filterObj[_filterKey]=Array.from(document.querySelectorAll('input[data-filter='+_filterKey+']:checked')).map(function(el){
+			    return el.value;
+			});
+		});
+        console.log(_filterObj, 'obyekt');
 
-let checked_data = document.querySelectorAll('.filter-item-checkbox')
-let url = 'http://localhost:8000/api/v1.0/products/'
-let checked_body = []
+	document.querySelector('.filter-btn').addEventListener('click', ()=> {
 
-sendRequest = (data) =>{
-	const csrftoken = Cookies.get('csrftoken');
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-			'X-CSRFToken': csrftoken,
-        },
-        body: JSON.stringify(data)
-    }).then(response => response.json())
-    .then(data => {
-		console.log('success');
-		console.log(data);
-	})
-}
+			// Run Ajax
+			$.ajax({
+				url: "http://localhost:8000/api/v1.0/products/",
+				data: _filterObj,
+				dataType: 'json',
+				beforeSend:function(){
+					console.log('necesen');
+					$(".ajaxLoader").show();
+				},
+				success:function(res){
+					console.log(res);
+					$("#filteredProducts").html(res.data);
+					$(".ajaxLoader").hide();
+				},
+				error: function(res){
+					console.log(res, 'error');
+				}
+			});
+		})
 
-document.querySelector('.filter-btn').addEventListener('click', ()=> {
-	for (let i = 0; i < checked_data.length; i++){
-		if (checked_data[i].checked) {
-			// console.log(checked_data[i].value);
-			checked_body.push({'title': checked_data[i].value})
-			// console.log(checked_body);
+
+	});
+
+});
+
+// let checked_data = document.querySelectorAll('.filter-item-checkbox')
+// let url = 'http://localhost:8000/api/v1.0/products/'
+// let checked_body = []
+
+// sendRequest = (data) =>{
+// 	// const csrftoken = Cookies.get('csrftoken');
+//     fetch(url, {
+//         method: "GET",
+//         headers: {
+//             "Content-type": "application/json",
+// 			// 'X-CSRFToken': csrftoken,
+//         },
+//         body: JSON.stringify(data)
+//     }).then(response => response.json())
+//     .then(data => {
+// 		console.log('success');
+// 		console.log(data);
+// 	})
+// }
+
+// document.querySelector('.filter-btn').addEventListener('click', ()=> {
+// 	for (let i = 0; i < checked_data.length; i++){
+// 		if (checked_data[i].checked) {
+// 			// console.log(checked_data[i].value);
+// 			checked_body.push({'title': checked_data[i].value})
+// 			// console.log(checked_body);
 			
-		}
-	}
-	sendRequest(checked_body)
-})
+// 		}
+// 	}
+// 	sendRequest(checked_body)
+// })
